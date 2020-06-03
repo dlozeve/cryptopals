27539840e33e6a5d74a28a677aef79cc3bc78523
Dimitri Lozeve <dimitri@lozeve.com>
2020-06-03 21:18:40 +0200

    Solve challenge 4

---
+++ ex04/main.c
@@ -0,0 +1,63 @@
+#include "utils.h"
+#include <ctype.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Usage: %s <filename>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  FILE *fp = fopen(argv[1], "r");
+  if (fp == NULL) {
+    printf("Error opening file %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+
+  printf("line | key | score | cleartext\n");
+  printf("-----+-----+-------+-------------------------------\n");
+
+  unsigned int line_count = 0;
+  char *line = NULL;
+  size_t line_length = 0;
+  ssize_t nread = 0;
+  size_t len;
+
+  while ((nread = getline(&line, &line_length, fp)) != -1) {
+    line_count++;
+
+    line[60] = '\0';
+    // printf("%3u: %s\n", line_count, line);
+    unsigned char buf[512] = {0};
+    len = hex_to_bytes(buf, line);
+
+    unsigned char cur[512] = {'\0'};
+    for (char c = 32; c < 127; ++c) {
+      for (size_t i = 0; i < len; ++i) {
+        cur[i] = buf[i] ^ c;
+      }
+      double score = frequency_score(cur, len);
+      if (score < 40.0) {
+        printf("%4u | %c   | %5.1f | ", line_count, c,
+               score);
+        for (size_t i = 0; i < len; ++i) {
+	  if (isprint(cur[i])) {
+	    putchar(cur[i]);
+	  } else if (cur[i] == '\n') {
+	    printf("\\n");
+	  }
+        }
+	printf("\n");
+      }
+    }
+  }
+
+  free(line);
+
+  if (fclose(fp)) {
+    printf("Error closing file %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
--- utils.c
+++ utils.c
@@ -38,15 +38,24 @@ unsigned char *bytes_to_base64(unsigned char *out,
 }
 
 double frequency_score(unsigned char *buf, size_t len) {
-  static const double english_freqs[26] = {
+  static const double english_freqs[27] = {
       0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, // A-G
       0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749, // H-N
       0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, // O-U
-      0.00978, 0.02360, 0.00150, 0.01974, 0.00074                    // V-Z
+      0.00978, 0.02360, 0.00150, 0.01974, 0.00074,                   // V-Z
+      0.19182                                                        // space
   };
 
-  unsigned int counts[26] = {0};
+  unsigned int counts[27] = {0};
   for (size_t i = 0; i < len; ++i) {
+    if (!isprint(buf[i]) && buf[i] != '\n') {
+      // If character is not printable, it's definitely not English.
+      return INFINITY;
+    }
+    if (buf[i] == ' ') {
+      counts[26]++;
+      continue;
+    }
     unsigned char c = tolower(buf[i]) - 'a';
     if (c < 26) {
       counts[c]++;
@@ -54,7 +63,7 @@ double frequency_score(unsigned char *buf, size_t len) {
   }
 
   double chi2 = 0;
-  for (size_t i = 0; i < 26; ++i) {
+  for (size_t i = 0; i < 27; ++i) {
     double expected = len * english_freqs[i];
     chi2 += pow(counts[i] - expected, 2) / expected;
   }
